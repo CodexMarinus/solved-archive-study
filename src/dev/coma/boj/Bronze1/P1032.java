803e4f4131978f0ce79a423efc817488f273de15
CoMa <comasocean@gmail.com>
2026-03-15 21:02:20 +0900

    260315

---
+++ src/dev/coma/boj/Bronze1/P1032.java
@@ -0,0 +1,23 @@
+package dev.coma.boj.Bronze1;
+
+import java.util.Scanner;
+
+public class P1032 {
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      int fileCount = Integer.parseInt(scanner.nextLine());
+
+      String[] fileNameArr = scanner.nextLine().split("");
+
+      for (int i = 1; i < fileCount; i++) {
+        String[] inputArr = scanner.nextLine().split("");
+
+        for (int j = 0; j < fileNameArr.length; j++) {
+          if (!fileNameArr[j].equals(inputArr[j])) fileNameArr[j] = "?";
+        }
+      }
+
+      System.out.println(String.join("", fileNameArr));
+    }
+  }
+}
