e253c4a43871858c8bb7b24e0c7ff1c745b3af88
Rayne <pluvstream@gmail.com>
2026-03-20 23:04:15 +0900

    260320

---
+++ src/dev/coma/boj/Bronze4/P34750.java
@@ -0,0 +1,19 @@
+package dev.coma.boj.Bronze4;
+
+import java.util.Scanner;
+
+public class P34750 {
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      int input = scanner.nextInt();
+
+      int parentMoney = 0;
+      if (input >= 1000000) parentMoney = (int) (input * 0.2);
+      else if (input >= 500000) parentMoney = (int) (input * 0.15);
+      else if (input >= 100000) parentMoney = (int) (input * 0.1);
+      else parentMoney = (int) (input * 0.05);
+
+      System.out.println(parentMoney + " " + (input - parentMoney));
+    }
+  }
+}
